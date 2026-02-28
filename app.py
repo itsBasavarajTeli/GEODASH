d49819bdb5b58971dd57583fb60f2932c2739473
itsBasavarajTeli <telib537@gmail.com>
2026-02-28 11:33:40 +0530

    Render deployment port fix

--- app.py
+++ app.py
@@ -1526,4 +1526,5 @@ def api_route():
 
 
 if __name__ == "__main__":
- app.run(host="127.0.0.1", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
